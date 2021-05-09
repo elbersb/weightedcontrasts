eae604c55c58b2828a6d624be0099450f4160482
Benjamin Elbers <elbersb@gmail.com>
2021-05-09 11:09:34 +0200

    wordsum data

--- R/data.R
+++ R/data.R
@@ -12,3 +12,10 @@
 #'  The Estimation of Age, Period and Cohort Effects for Vital Rates.
 #'  Biometrics 39(2): 311-324.
 "prostate"
+
+#' @title Data from apcR package.
+#' @description Dataset used by Fosse and Winship 2019
+#' @source Fosse, Ethan and Christopher Winship. 2019.
+#'   Analyzing Age-Period-Cohort Data: A Review and Critique,
+#'   Annual Review of Sociology 45:467–92.
+"wordsum"
